eab917663295a51315d5036abab241e584bad3f3
yuya-kambayashi <kambayashi73@gmail.com>
2025-03-14 00:28:25 +0900

    first commit

---
+++ src/main/java/com/bluejeanssystems/backlog/BacklogApplication.java
@@ -0,0 +1,13 @@
+package com.bluejeanssystems.backlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacklogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BacklogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bluejeanssystems/backlog/BacklogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluejeanssystems.backlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacklogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
