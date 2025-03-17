54655752c76f05bb0e0c83cc122ef6fe5f1a2587
yuya-kambayashi <kambayashi73@gmail.com>
2025-03-17 14:11:55 +0900

    課題の追加

--- src/main/java/com/bluejeanssystems/backlog/controller/ProjectController.java
+++ src/main/java/com/bluejeanssystems/backlog/controller/ProjectController.java
@@ -1,10 +1,15 @@
 package com.bluejeanssystems.backlog.controller;
 
+import com.bluejeanssystems.backlog.model.Issue;
 import com.bluejeanssystems.backlog.repository.IssueRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
@@ -19,9 +24,20 @@ public class ProjectController {
         return "layout/home";
     }
     @GetMapping("/add")
-    public String add(Model model) {
-        model.addAttribute("issues", issueRepository.findAll());
+    public String add(@ModelAttribute("issue")Issue issue) {
         return "layout/add";
+    }
+    @PostMapping("/add")
+    public String add(@Validated @ModelAttribute("issue")Issue issue,
+                      BindingResult result) {
+        if (result.hasErrors()) {
+            return "layout/add";
+        }
+
+        issueRepository.save(issue);
+
+        //return "redirect:/projects/view";
+        return "redirect:/projects/home";
     }
     @GetMapping("/find")
     public String find(Model model) {
