85eb275b3f36e618d7784b3f07861ee90541e3b8
yuya-kambayashi <kambayashi73@gmail.com>
2025-03-20 00:35:28 +0900

    statusによる絞り込み

--- src/main/java/com/bluejeanssystems/backlog/controller/FindController.java
+++ src/main/java/com/bluejeanssystems/backlog/controller/FindController.java
@@ -1,53 +1,35 @@
 package com.bluejeanssystems.backlog.controller;
 
 import com.bluejeanssystems.backlog.repository.IssueRepository;
-import com.bluejeanssystems.backlog.util.Status;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/projects/find")
+@RequestMapping("/projects")
 public class FindController {
     private final IssueRepository issueRepository;
 
-    @GetMapping("/")
-    public String find(Model model,
-                       @RequestParam(name = "statusId")Integer type) {
-        // statusId
-        // 1：すべて
-        // 2：未対応
-        // 3：処理中
-        // 4：処理中
-        // 5：完了
-        // 6：完了以外
-        switch (type) {
-            case 1:
-                model.addAttribute("issues", issueRepository.findAll());
-                break;
-            case 2:
-                model.addAttribute("issues", issueRepository.findByStatus(Status.未対応));
-                break;
-            case 3:
-                model.addAttribute("issues", issueRepository.findByStatus(Status.処理中));
-                break;
-            case 4:
-                model.addAttribute("issues", issueRepository.findByStatus(Status.処理済み));
-                break;
-            case 5:
-                model.addAttribute("issues", issueRepository.findByStatus(Status.完了));
-                break;
-            case 6:
-                var issues = issueRepository.findByStatus(Status.未対応);
-                issues.addAll(issueRepository.findByStatus(Status.処理中));
-                issues.addAll(issueRepository.findByStatus(Status.処理済み));
-                model.addAttribute("issues", issues);
-                break;
-        }
+    @GetMapping("/find")
+    public String find(
+            @RequestParam(required = false) Integer status,
+            @RequestParam(required = false) List<String> type,
+            Model model
+    ) {
+        var issues = issueRepository.findAll();
+
+        var result = issues.stream()
+                        .filter(issue -> (status == null  || status == issue.getStatus().ordinal()))
+                        .collect(Collectors.toList());
+
+        model.addAttribute("issues", result);
+
 
         return "layout/find";
     }
