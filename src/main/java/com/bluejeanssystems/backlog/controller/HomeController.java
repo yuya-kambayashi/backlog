d2a0a761467e928c677b3f9204fe2d0d4c8355fc
yuya-kambayashi <kambayashi73@gmail.com>
2025-04-13 23:15:24 +0900

    最新の更新の内容を調整

--- src/main/java/com/bluejeanssystems/backlog/controller/HomeController.java
+++ src/main/java/com/bluejeanssystems/backlog/controller/HomeController.java
@@ -1,7 +1,9 @@
 package com.bluejeanssystems.backlog.controller;
 
+import com.bluejeanssystems.backlog.model.TransactionLog;
 import com.bluejeanssystems.backlog.repository.IssueRepository;
 import com.bluejeanssystems.backlog.repository.ProjectRepository;
+import com.bluejeanssystems.backlog.repository.TransactionLogRepository;
 import com.bluejeanssystems.backlog.util.Status;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -10,12 +12,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/projects/{projectKey}")
 public class HomeController {
     private final IssueRepository issueRepository;
     private final ProjectRepository projectRepository;
+    private final TransactionLogRepository transactionLogRepository;
 
     @GetMapping("/home")
     public String home(@PathVariable("projectKey") String projectKey, Model model) {
@@ -30,9 +37,23 @@ public class HomeController {
         model.addAttribute("処理済み数", issues.stream().filter(issue -> issue.getStatus() == Status.処理済み).count());
         model.addAttribute("完了数", issues.stream().filter(issue -> issue.getStatus() == Status.完了).count());
 
+        var logs = transactionLogRepository.findAll();
+        var map = new LinkedHashMap<String, List<TransactionLog>>();
+        logs = logs.stream().filter(log -> log.getIssue().getProject().getProjectKey().equals(projectKey)).toList();
+        for (var log : logs) {
+            if (map.containsKey(log.getDate())) {
+                var list = map.get(log.getDate());
+                list.add(log);
+                map.put(log.getDate(), list);
+            } else {
+                var list = new ArrayList<TransactionLog>();
+                list.add(log);
+                map.put(log.getDate(), list);
+            }
+        }
+
+        model.addAttribute("logMap", map);
 
         return "layout/home";
     }
-
-
 }
