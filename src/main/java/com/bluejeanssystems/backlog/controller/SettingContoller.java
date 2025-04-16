ee4157fa46e2d7af0f7c3ade3466d16a0a877e13
yuya-kambayashi <kambayashi73@gmail.com>
2025-04-16 17:42:43 +0900

    設定にマイルストーンの一覧を追加

--- src/main/java/com/bluejeanssystems/backlog/controller/SettingContoller.java
+++ src/main/java/com/bluejeanssystems/backlog/controller/SettingContoller.java
@@ -1,5 +1,7 @@
 package com.bluejeanssystems.backlog.controller;
 
+import com.bluejeanssystems.backlog.repository.MilestoneRepository;
+import com.bluejeanssystems.backlog.repository.ProjectRepository;
 import com.bluejeanssystems.backlog.repository.SiteUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -13,12 +15,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/projects/{projectKey}")
 public class SettingContoller {
     private final SiteUserRepository siteUserRepository;
+    private final MilestoneRepository milestoneRepository;
+    private final ProjectRepository projectRepository;
 
 
     @GetMapping("/setting")
     public String view(@PathVariable("projectKey") String projectKey,
                        Model model) {
+        var project = projectRepository.findByProjectKey(projectKey);
+
         model.addAttribute("users", siteUserRepository.findAll());
+        model.addAttribute("milestones", milestoneRepository.findAllBy(project.getId()));
 
         return "layout/setting";
     }
